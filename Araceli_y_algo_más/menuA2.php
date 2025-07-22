ab1ecc96c3c33f123c28de16eb55fb0333a3406a
Nayes <naye01y8@gmail.com>
2025-07-22 15:49:51 -0600

    Se cambian el nombre de las 3 categorias a todos

--- Araceli_y_algo_más/menuA2.php
+++ Araceli_y_algo_más/menuA2.php
@@ -48,21 +48,21 @@ $resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
 
         <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
 
-          <li><a href="index.php" class="nav-link px-3 text" style="color: #6E0023; display:inline; border-right: 2px solid  #78b36f">INICIO</a>
+          <li><a href="index.php" class="nav-link px-3 text" style="color: #6E0023; display:inline; border-right: 2px solid  #f0cea5">INICIO</a>
           </li>
 
 
           <li>
-            <a class="nav-link dropdown-toggle" style=" color:#6E0023; display:inline;  border-right: 2px solid  #78b36f" href=" #" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <a class="nav-link dropdown-toggle" style=" color:#6E0023; display:inline;  border-right: 2px solid  #f0cea5" href=" #" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               MENÚ
             </a>
             <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <li><a class="dropdown-item" style="color: #6E0023;" href="menuC2.php">Bebidas Calientes</a></li>
-              <li><a class="dropdown-item" style="color: #6E0023;" href="menuF2.php">Bebidas Frías</a></li>
-              <li><a class="dropdown-item" style="color: #6E0023;" href="menuA2.php">Alimentos</a></li>
+              <li><a class="dropdown-item" style="color: #6E0023;" href="menuC2.php">Herbales</a></li>
+              <li><a class="dropdown-item" style="color: #6E0023;" href="menuF2.php">Nutricionales</a></li>
+              <li><a class="dropdown-item" style="color: #6E0023;" href="menuA2.php">Nutricosmenticos</a></li>
             </ul>
           </li>
-          <li><a href="VerEventosGabcy.php" class="nav-link px-3 text" style="color: #6E0023; display:inline; border-right: 2px solid  #78b36f;">EVENTOS</a>
+          <li><a href="VerEventosGabcy.php" class="nav-link px-3 text" style="color: #6E0023; display:inline; border-right: 2px solid  #f0cea5;">EVENTOS</a>
           </li>
 
           <li><a href="conocenos.php" class="nav-link px-3 text" style=" color: #6E0023; display:inline; ">ACERCA
@@ -96,7 +96,7 @@ $resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
               <ul class="dropdown-menu text-small" style=" font-family:Monserrat, sans-serif;  color: #6E0023;">
                <li><a class="dropdown-item" style=" font-family:Monserrat, sans-serif;  color: #6E0023;" disbled>Cliente...</a></li>
                <li><a class="dropdown-item" style=" font-family:Monserrat, sans-serif;  color: #6E0023;" href="perfil.php"> Mi Perfil</a></li>
-               <hr class="dropdown-divider" style="color: #78b36f">
+               <hr class="dropdown-divider" style="color: #f0cea5">
                <li><a class="dropdown-item" style=" font-family:Monserrat, sans-serif;  color: #6E0023;" href="destroySesion.php">Cerrar Sesión</a></li>';
               }
               if ($_SESSION['permiso'] == 2) {
@@ -110,7 +110,7 @@ $resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
              <li><a class="dropdown-item" style=" font-family:Monserrat, sans-serif;  color: #6E0023;" disbled>Empleado...</a></li>
              <li><a class="dropdown-item" style=" font-family:Monserrat, sans-serif;  color: #6E0023;" href="Menu_empleado.php">Menú Empleado</a></li>
              <li><a class="dropdown-item" style=" font-family:Monserrat, sans-serif;  color: #6E0023;" href="perfil.php"> Mi Perfil</a></li>
-             <hr class="dropdown-divider" style="color: #78b36f">
+             <hr class="dropdown-divider" style="color: #f0cea5">
              <li><a class="dropdown-item" style=" font-family:Monserrat, sans-serif;  color: #6E0023;" href="destroySesion.php">Cerrar Sesión</a></li>';
               }
               if ($_SESSION['permiso'] == 3) {
@@ -124,7 +124,7 @@ $resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
            <li><a class="dropdown-item" style=" font-family:Monserrat, sans-serif;  color: #6E0023;" disbled>Administrador...</a></li>
            <li><a class="dropdown-item" style=" font-family:Monserrat, sans-serif;  color: #6E0023;" href="MenuAdmn.php">Menú Administrador</a></li>
            <li><a class="dropdown-item" style=" font-family:Monserrat, sans-serif;  color: #6E0023;" href="perfil.php"> Mi Perfil</a></li>
-           <hr class="dropdown-divider" style="color: #78b36f">
+           <hr class="dropdown-divider" style="color: #f0cea5">
            <li><a class="dropdown-item" style=" font-family:Monserrat, sans-serif;  color: #6E0023;" href="destroySesion.php">Cerrar Sesión</a></li>';
               }
             } else {
@@ -136,7 +136,7 @@ $resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
 
                 <ul class="dropdown-menu text-small" style=" font-family:'Monserrat', sans-serif;  color: #6E0023;">
                   <li><a class="dropdown-item" style=" font-family:'Monserrat', sans-serif;  color: #6E0023;" href="inicioSesion.php">Iniciar Sesión</a></li>
-                  <hr class="dropdown-divider" style="color: #78b36f">
+                  <hr class="dropdown-divider" style="color: #f0cea5">
                   <li><a class="dropdown-item" style=" font-family:'Monserrat', sans-serif;  color: #6E0023;" href="registro.php">Crear Cuenta</a></li>
                 <?php
               }
