4d0d1632508011f1397722128871bc2d369d0dee
Nayes <naye01y8@gmail.com>
2025-07-22 15:18:26 -0600

    Se modifico el footer (creditos)

--- Araceli_y_algo_más/creditos.php
+++ Araceli_y_algo_más/creditos.php
@@ -103,10 +103,10 @@
             <form>
               <h5>Ubicación</h5>
               <div class="d-flex flex-column flex-sm-row w-100 gap-2">
-              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d120206.14300945461!2d-99.48095245664057!3d19.693798900000008!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d219815473b387%3A0x5383cc770847315!2sCafeter%C3%ADa%20Gabcy!5e0!3m2!1ses-419!2smx!4v1681980865811!5m2!1ses-419!2smx" width="320" height="250" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3753.1888642246595!2d-99.25337052610291!3d19.831960127808536!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d226e97b2cdeef%3A0xeafc0656ef2aca20!2sYerbabuena%206%2C%2054694%20Santa%20Teresa%2C%20M%C3%A9x.!5e0!3m2!1ses!2smx!4v1753218793664!5m2!1ses!2smx" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 
               </div>
-              <p>Calle Miguel Hidalgo 18, Cañada de Cisneros, CP 54650. EdoMex</p>
+              <p>Yerbabuena 6, 54694 Santa Teresa, Méx.</p>
             </form>
             <br>
 
@@ -124,9 +124,9 @@
             <h5>Contactanos</h5>
             <ul class="nav flex-column">
               <li class="nav-item mb-2">Email</li>
-              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-dark">cafeteriagabcy@gmail.com</a></li>
-              <li class="nav-item mb-2">What´s app</li>
-              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-dark">56-1260-3194</a></li>
+              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-dark">almarazaraceli777@gmail.com</a></li>
+              <li class="nav-item mb-2">WhatsApp</li>
+              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-dark">55-1260-3194</a></li>
             </ul>
           </div>
 
